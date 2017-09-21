5ee542f2038b2bfacf4d31397aa8da01b7659514
John Hauck <john_hauck@lecotc.com>
2017-09-21 09:31:14 -0400

    Slow restore 2

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var paragraphs = "";
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  fs.readFile("public/history_reformation.txt", 'utf8', function (err, fileText) { res.send(convertBook(fileText)) });
+  fs.readFile("history_reformation.txt", 'utf8', function (err, fileText) { res.send(convertBook(fileText)) });
   //res.send("Howdy James".replace("James", "Fred"));
 });
 
