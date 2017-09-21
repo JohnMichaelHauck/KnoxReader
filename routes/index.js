633bee0865be406487e692fc2d4407a836233b89
John Hauck <john_hauck@lecotc.com>
2017-09-21 10:14:37 -0400

    Slow restore 7

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,7 @@ var paragraphs = "";
 /* GET home page. */
 router.get('/', function (req, res, next) {
   var fileName = __dirname.replace('routes','public') + "\\history_reformation.txt";
-  var exists = fs.existsSync( fileName );
-  res.send(fileName + ' ---  exists: ' + exists);
-  //fs.readFile( fileName, 'utf8', function (err, fileText) { res.send(convertBook(fileText)) });
+  fs.readFile( fileName, 'utf8', function (err, fileText) { res.send(convertBook(fileText)) });
 });
 
 var encodingArray = [];
